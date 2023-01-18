3f00a78056ec98d3aae21d306e0ae2e45b8546f0
TremblingV5 <Administrator@XinZF>
2023-01-18 23:07:42 +0800

    gorm/gen生成关于数据库操作的相关代码，完成一些基本代码以及获取视频点赞数和评论数的代码

---
+++ applications/feed/dal/service/var.go
@@ -0,0 +1,5 @@
+package service
+
+import "gorm.io/gorm"
+
+var DB *gorm.DB
---
+++ applications/feed/handler/get_video_by_id.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"context"
+
+	"github.com/TremblingV5/DouTok/kitex_gen/feed"
+)
+
+func (s *FeedServiceImpl) GetVideoById(ctx context.Context, req *feed.DouyinFeedRequest) (resp *feed.DouyinFeedResponse, err error) {
+	return
+}
