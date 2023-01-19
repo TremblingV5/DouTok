fab2ee489d6ba319070911a6357601e7651f9f44
TremblingV5 <Administrator@XinZF>
2023-01-19 23:18:21 +0800

    简单更改了已有的代码，确定一个plan作为后续将要实现的主要功能点

--- applications/publish/main.go
+++ applications/publish/main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/TremblingV5/DouTok/applications/publish/service"
+)
+
+func InitDb() {
+	service.InitDb()
+}
+
+func main() {
+	InitDb()
+	v, _ := service.QueryVideoFromRBDById(1)
+	vs, _ := service.QuerySomeVideoFromRDBByIds(1, 22)
+	fmt.Println(v)
+
+	for _, v := range vs {
+		fmt.Println(v)
+	}
+
+	fmt.Println(service.DB)
+}
