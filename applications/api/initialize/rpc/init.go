40f0d9a0098719927d3f3cdeb3a34fadca7b621c
IsJulia <2000300802@mails.guet.edu.cn>
2023-01-30 09:05:52 +0800

    忘记同步了，这次是希望之后能rebase

---
+++ applications/api/initialize/rpc/init.go
@@ -0,0 +1,29 @@
+package rpc
+
+import (
+	"github.com/TremblingV5/DouTok/pkg/dtviper"
+)
+
+// InitRPC init rpc client
+func InitRPC(Config *dtviper.Config) {
+	UserConfig := dtviper.ConfigInit("DOUTOK_USER", "user")
+	initUserRpc(&UserConfig)
+
+	FeedConfig := dtviper.ConfigInit("DOUTOK_FEED", "feed")
+	initFeedRpc(&FeedConfig)
+
+	PublishConfig := dtviper.ConfigInit("DOUTOK_PUBLISH", "publish")
+	initPublishRpc(&PublishConfig)
+
+	FavoriteConfig := dtviper.ConfigInit("DOUTOK_FAVORITE", "favorite")
+	initFavoriteRpc(&FavoriteConfig)
+
+	CommentConfig := dtviper.ConfigInit("DOUTOK_COMMENT", "comment")
+	initCommentRpc(&CommentConfig)
+
+	RelationConfig := dtviper.ConfigInit("DOUTOK_RELATION", "relation")
+	initRelationRpc(&RelationConfig)
+
+	MessageConfig := dtviper.ConfigInit("DOUTOK_MESSAGE", "message")
+	initMessageRpc(&MessageConfig)
+}
---
+++ documents/pkgs/grafana.md
@@ -0,0 +1,8 @@
+## Grafana 监控平台
+
+- 使用 VM 替换 Prometheus
+  https://zyun.360.cn/blog/?p=1536
+- Grafana 数据源 URL 配置问题
+  https://blog.csdn.net/qq_38366063/article/details/128570644
+- DashBoard 展示模板的配置
+  https://blog.csdn.net/ll535299/article/details/127534678
