c5bd1b8015320c447a4b10ba1171bce7a7a2b6e0
Xminry <xmrsunmoon@foxmail.com>
2023-01-31 19:40:57 +0800

    feat:表结构与接口

--- applications/comment/dao/dao.go
+++ applications/comment/dao/dao.go
@@ -1,9 +1,29 @@
 package dao
 
+import "gorm.io/gorm"
+
 type CommentOperation interface {
 	AddComment(comment Comment) error
 
 	DelComment(comment Comment) error
 
-	GetVideoComment(videoId int64) []Comment
+	GetVideoComment(vedioId int64) []Comment
+}
+
+type GormDB struct {
+	gorm.DB
+}
+
+func (g *GormDB) AddComment(comment Comment) error {
+	return g.Create(&comment).Error
+}
+
+func (g *GormDB) DelComment(comment Comment) error {
+	return g.Delete(&comment).Error
+}
+
+func (g *GormDB) GetVideoComment(vedioId int64) []Comment {
+	var cList []Comment
+	g.Find(&cList, "vedio_id=?", vedioId)
+	return cList
 }
