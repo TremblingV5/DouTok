f830c50dbd274c57689a4bb86298615fb5f71ca7
TremblingV5 <1340349413@qq.com>
2023-01-31 21:59:50 +0800

    对push做了简单修改，具备一次push多个的功能

--- pkg/redisHandle/push.go
+++ pkg/redisHandle/push.go
@@ -2,10 +2,10 @@ package redishandle
 
 import "context"
 
-func (c *RedisClient) LPush(ctx context.Context, key string, value string) error {
+func (c *RedisClient) LPush(ctx context.Context, key string, value ...string) error {
 	return c.Client.LPush(ctx, key, value).Err()
 }
 
-func (c *RedisClient) RPush(ctx context.Context, key string, value string) error {
+func (c *RedisClient) RPush(ctx context.Context, key string, value ...string) error {
 	return c.Client.RPush(ctx, key, value).Err()
 }
