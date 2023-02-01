b207a5cdc269f239ed7220a51eba8e459216f5b3
unknown <1451763509@qq.com>
2023-02-01 21:48:55 +0800

    redis opt

---
+++ applications/relation/handler/relation_follower_list.go
@@ -0,0 +1,11 @@
+package handler
+
+import (
+	"context"
+	"github.com/TremblingV5/DouTok/kitex_gen/relation"
+)
+
+func (s *RelationServiceImpl) RelationFollowerList(ctx context.Context, req *relation.DouyinRelationFollowerListRequest) (resp *relation.DouyinRelationFollowerListResponse, err error) {
+	// TODO: Your code here...
+	return
+}
