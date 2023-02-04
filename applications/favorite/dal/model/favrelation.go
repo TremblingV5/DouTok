5bdd2a56b73c0e82d99f1b6b556f404f14660533
IsJulia <2000300802@mails.guet.edu.cn>
2023-02-04 21:31:56 +0800

    modify the content of favorite

---
+++ applications/favorite/dal/model/favrelation.go
@@ -0,0 +1,7 @@
+package model
+
+type FavRelation struct {
+	UserId  uint64
+	VideoId uint64
+	Status  bool
+}
---
+++ applications/favorite/dal/model/videocount.go
@@ -0,0 +1,6 @@
+package model
+
+type VideoCount struct {
+	VideoId  uint64
+	FavCount uint64
+}
