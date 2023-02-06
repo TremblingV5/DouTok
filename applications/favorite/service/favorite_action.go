01d5edc78e3280454169af9885668470f3af2cc7
IsJulia <2000300802@mails.guet.edu.cn>
2023-02-06 17:15:07 +0800

    修改service部分代码，增加handler部分代码

--- applications/favorite/service/favorite_action.go
+++ applications/favorite/service/favorite_action.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"github.com/TremblingV5/DouTok/applications/favorite/dal/model"
+	"github.com/TremblingV5/DouTok/kitex_gen/favorite"
+	"gorm.io/gorm"
 )
 
 /*
@@ -14,17 +16,26 @@ type FavRelation struct {
 */
 
 // 记录点赞状态
-func SaveFavRelation(user_id int64, video_id int64) error {
+// 对于取消点赞的，不删对应行信息，而是追加一条新纪录，查询的时候以最新一行信息为准
+func SaveFavRelation(req *favorite.DouyinFavoriteActionRequest) error {
 
 	// 2. 写入数据到MySQl
+	var op bool
+	if req.ActionType == 1 {
+		op = true
+	}
+	if req.ActionType == 2 {
+		op = false
+	}
 	_, err := SaveFavRelation2DB(
-		uint64(user_id), uint64(video_id), bool(true))
+		uint64(req.UserId), uint64(req.VideoId), bool(op))
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
+// 保存点赞状态信息到FavRelation表中,并更改VideoCount表点赞数量
 func SaveFavRelation2DB(user_id uint64, video_id uint64, op bool) (uint64, error) {
 	newfavRelation := model.FavRelation{
 		UserId:  user_id,
@@ -36,6 +47,17 @@ func SaveFavRelation2DB(user_id uint64, video_id uint64, op bool) (uint64, error
 
 	if err != nil {
 		return 0, err
+	}
+
+	var res *gorm.DB
+	if op == true {
+		res = DB.Model(VideoCount).Update("fav_count", gorm.Expr("fav_count + ?", 1))
+	} else {
+		res = DB.Model(VideoCount).Update("fav_count", gorm.Expr("fav_count - ?", 1))
+	}
+
+	if res.Error != nil {
+		return newfavRelation.UserId, res.Error
 	}
 
 	return newfavRelation.UserId, nil
