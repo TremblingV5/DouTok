7c30e1dc9b4a77ba1aaac54911fd2539d6e30dce
TremblingV5 <zf.xin@qq.com>
2023-02-19 09:40:39 +0800

    fav count set 0 after saving to db

--- applications/favorite/service/timer.go
+++ applications/favorite/service/timer.go
@@ -10,7 +10,11 @@ import (
 func UpdateFavMap() {
 	time.Sleep(time.Duration(5) * time.Second)
 
+	keyList := []string{}
+
 	FavCount.Iter(func(key string, v interface{}) {
+		keyList = append(keyList, key)
+
 		key_i64, _ := strconv.ParseInt(key, 10, 64)
 		err := UpdateCount(key_i64, v.(int64))
 		if err != nil {
@@ -18,9 +22,15 @@ func UpdateFavMap() {
 			dlog.Warn("Write favourite count to RDB defeat: " + key + " with count: " + v.(string))
 		}
 
+		FavCount.Set(key, 0)
+
 		err = DelCount2Cache(key_i64)
 		if err != nil {
 			dlog.Warn("Delete favourite count from third party cache defeat: " + key)
 		}
 	})
+
+	for _, v := range keyList {
+		FavCount.Set(v, 0)
+	}
 }
