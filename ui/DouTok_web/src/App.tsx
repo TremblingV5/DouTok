830a12b6606c4e96aca4aa6933906356da893310
@wang-yixian123 <906950106@qq.com>
2024-02-26 20:17:23 +0800

    web-init

---
+++ ui/DouTok_web/src/App.tsx
@@ -0,0 +1,22 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Hello DouTok</h1>
+    </>
+  )
+}
+
+export default App
