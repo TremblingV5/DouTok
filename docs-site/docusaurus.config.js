ccc2c12f9299c5410c8d54e9b3c4ea663ea80d19
Xin ZF <43563921+TremblingV5@users.noreply.github.com>
2024-03-04 13:36:50 +0800

    Update docusaurus.config.js
    Signed-off-by: Xin ZF <43563921+TremblingV5@users.noreply.github.com>

--- docs-site/docusaurus.config.js
+++ docs-site/docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://tremblingv5.github.io',
+  url: 'https://doutok.zhengfei.xin',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/DouTok/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
