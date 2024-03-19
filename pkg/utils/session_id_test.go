4775183f1f6f7ab99068e22368e6fd010959099f
issuelMel <2291381998@qq.com>
2024-03-19 17:17:44 +0800

    change to t.Run() with test name

--- pkg/utils/session_id_test.go
+++ pkg/utils/session_id_test.go
@@ -6,42 +6,46 @@ import (
 )
 
 func TestGenerateSessionIdShouldEqual(t *testing.T) {
-	testCase := []struct {
+	cases := []struct {
+		name string
 		a, b int64
 		c    string
-		info string
 	}{
-		{100, 200, "100200",
-			"GenerateSessionId with two positive numbers"},
-		{-1, -1, "-1-1",
-			"GenerateSessionId with two negative numbers"},
-		{1<<63 - 1, 1<<63 - 1, "92233720368547758079223372036854775807",
-			"GenerateSessionId with two MaxInt64"},
-		{1<<63 - 1, -1 << 63, "-92233720368547758089223372036854775807",
-			"GenerateSessionId with MaxInt64 and MinInt64"},
+		{"GenerateSessionId with two positive numbers",
+			100, 200, "100200"},
+		{"GenerateSessionId with two negative numbers",
+			-1, -1, "-1-1"},
+		{"GenerateSessionId with two MaxInt64",
+			1<<63 - 1, 1<<63 - 1, "92233720368547758079223372036854775807"},
+		{"GenerateSessionId with MaxInt64 and MinInt64",
+			1<<63 - 1, -1 << 63, "-92233720368547758089223372036854775807"},
 	}
-	for _, tt := range testCase {
-		require.Equal(t, GenerateSessionId(tt.a, tt.b), tt.c, tt.info)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			require.Equal(t, GenerateSessionId(c.a, c.b), c.c)
+		})
 	}
 }
 
 func TestGenerateSessionIdShouldNotEqual(t *testing.T) {
-	testCase := []struct {
+	cases := []struct {
+		name string
 		a, b int64
 		c    string
-		info string
 	}{
-		{100, 200, "111222",
-			"GenerateSessionId with two positive numbers"},
-		{-1, -1, "-11",
-			"GenerateSessionId with two negative numbers, case 1"},
-		{-1, -1, "1-1",
-			"GenerateSessionId with two negative numbers, case 2"},
-		{1<<63 - 1, -1 << 63, "9223372036854775807-9223372036854775808",
-			"GenerateSessionId with MaxInt64 and MinInt64"},
+		{"GenerateSessionId with two positive numbers",
+			100, 200, "111222"},
+		{"GenerateSessionId with two negative numbers, case 1",
+			-1, -1, "-11"},
+		{"GenerateSessionId with two negative numbers, case 2",
+			-1, -1, "1-1"},
+		{"GenerateSessionId with MaxInt64 and MinInt64",
+			1<<63 - 1, -1 << 63, "9223372036854775807-9223372036854775808"},
 	}
 
-	for _, tt := range testCase {
-		require.NotEqual(t, GenerateSessionId(tt.a, tt.b), tt.c, tt.info)
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			require.NotEqual(t, GenerateSessionId(c.a, c.b), c.c)
+		})
 	}
 }
